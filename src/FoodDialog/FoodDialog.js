c6d563931a1d9507c8bdba0b4317f0e5ca955fbd
Nanette <nanette.codes@gmail.com>
2020-02-14 12:26:12 -0800

    qauntity

--- src/FoodDialog/FoodDialog.js
+++ src/FoodDialog/FoodDialog.js
@@ -4,6 +4,8 @@ import { FoodLabel } from '../Menu/FoodGrid';
 import { pizzaRed } from '../Styles/colors';
 import { Title } from '../Styles/title';
 import { formatPrice } from '../Data/FoodData';
+import { QuantityInput } from './QuantityInput';
+import { useQuantity } from '../Hooks/useQuantity';
 
 const Dialog = styled.div`
     width: 500px;
@@ -16,8 +18,12 @@ const Dialog = styled.div`
     display: flex;
     flex-direction: column;
 `;
+
 export const DialogContent = styled.div`
     overflow: auto;
+    min-height: 100px;
+    padding: 0px 40px;
+    padding-bottom: 80px;
 `;
 
 export const DialogFooter = styled.div`
@@ -28,15 +34,15 @@ export const DialogFooter = styled.div`
 `;
 
 export const ConfirmButton = styled(Title)`
-margin: 10px;
-color: white; 
-height: 20px; 
-border-radius: 5px;
-padding: 10px; 
-text-align: center; 
-width: 200px; 
-background-color: ${pizzaRed};
-cursor: pointer;
+    margin: 10px;
+    color: white;
+    height: 20px;
+    border-radius: 5px;
+    padding: 10px;
+    text-align: center;
+    width: 200px;
+    cursor: pointer;
+    background-color: ${pizzaRed};
 `;
 
 const DialogShadow = styled.div`
@@ -46,30 +52,44 @@ const DialogShadow = styled.div`
     top: 0px;
     background-color: black;
     opacity: 0.7;
-    z-index:4;
+    z-index: 4;
 `;
 
 const DialogBanner = styled.div`
-min-height: 200px;
-margin-bottom: 20px;
-${({ img }) => `background-image: url(${img});`}
-background-position: center;
-background-size: cover;
+    min-height: 200px;
+    margin-bottom: 20px;
+    ${({ img }) => (img ? `background-image: url(${img});` : `min-height: 75px;`)}
+    background-position: center;
+    background-size: cover;
 `;
 
 const DialogBannerName = styled(FoodLabel)`
-    top: 100px;
     font-size: 30px;
     padding: 5px 40px;
+    top: ${({ img }) => (img ? `100px` : `20px`)};
 `;
 
-export function FoodDialog({ openFood, setOpenFood, setOrders, orders }) {
+const pricePerTopping = 0.5;
+
+export function getPrice(order) {
+    return order.quantity * order.price;
+    // return (
+    //     order.quantity *
+    //     (order.price +
+    //         order.toppings.filter(t => t.checked).length * pricePerTopping)
+    // );
+}
+
+function FoodDialogContainer({ openFood, setOpenFood, setOrders, orders }) {
+    const quantity = useQuantity(openFood && openFood.quantity);
+
     function close() {
         setOpenFood();
     }
-    if (!openFood) return null;
+
     const order = {
-        ...openFood
+        ...openFood,
+        quantity: quantity.value,
     };
 
     function addToOrder() {
@@ -85,14 +105,19 @@ export function FoodDialog({ openFood, setOpenFood, setOrders, orders }) {
                     <DialogBannerName>{openFood.name}</DialogBannerName>
                 </DialogBanner>
                 <DialogContent>
-
+                    <QuantityInput quantity={quantity} />
                 </DialogContent>
                 <DialogFooter>
                     <ConfirmButton onClick={addToOrder}>
-                        Add to order: {formatPrice(openFood.price)}
+                        Add to order: {formatPrice(getPrice(order))}
                     </ConfirmButton>
                 </DialogFooter>
             </Dialog>
         </>
     );
 };
+
+export function FoodDialog(props) {
+    if (!props.openFood) return null;
+    return <FoodDialogContainer {...props} />;
+}
